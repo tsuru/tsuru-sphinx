8aebd079ddb493154a861a18cdf0dccc502e8624
Cezar Sa Espinola <cezarsa@gmail.com>
2016-02-12 11:29:17 -0200

    initial commit

---
+++ setup.py
@@ -0,0 +1,28 @@
+# -*- coding: utf-8 -*-
+
+# Copyright 2016 tsuru authors. All rights reserved.
+# Use of this source code is governed by a BSD-style
+# license that can be found in the LICENSE file.
+from setuptools import setup, find_packages
+
+setup(
+    name="tsuru-sphinx",
+    url="https://github.com/tsuru/tsuru-sphinx",
+    version='0.1.0',
+    description="Sphinx extensions used in tsuru documentation",
+    author="Tsuru",
+    author_email="tsuru@corp.globo.com",
+    classifiers=[
+        "Programming Language :: Python :: 2.7",
+    ],
+    packages=find_packages(exclude=["docs", "tests"]),
+    include_package_data=True,
+    install_requires=[
+        "docutils==0.12",
+    ],
+    entry_points={
+        'console_scripts': [
+            'tsuru_sphinx=tsuru_sphinx:main',
+        ],
+    },
+)
